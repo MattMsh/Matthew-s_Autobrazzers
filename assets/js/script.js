057b7de721a31e7c8fe29896ac08cc7b08facdee
Matthew4Jesus <matvii.mashtalyar@gmail.com>
2019-12-24 14:12:52 +0300

    added adapt to last 3 blocks

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,7 +18,27 @@ $(function(){
   });
 
 $('.examples__slider').slick({
-infinite: true,
-slidesToShow: 3,
-slidesToScroll: 3
+	dots: true,
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 3,
+	responsive: [
+		{
+		  breakpoint: 1110,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	  ]
 });
