b76f727ca3f35a831789e6665c685a8cec09600c
Matthew4Jesus <matvii.mashtalyar@gmail.com>
2019-12-28 17:03:26 +0200

    added some new(services, selection_car, reviews, contacts)

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ $('.lang__text').on('click', function(e) {
 	e.preventDefault;
 	$(this).toggleClass('active');
 	$('.lang-switch').toggleClass('active');
-})
+});
 
 $('.header_menu').on('click', function(e) {
 	e.preventDefault;
@@ -11,11 +11,28 @@ $('.header_menu').on('click', function(e) {
 	$('.menu').toggleClass('active');
 	$('.menu_btn').toggleClass('active');
 	$('.menu_text').toggleClass('active');
-})
+});
 
 $(function(){
 	$("input[name=phone]").mask("+38 (099) 999 99 99");
   });
+
+$('.open-btn.max').on('click', function(e) {
+	e.preventDefault;
+	$(this).parent().toggleClass('active');
+	$('.tarif_text.max').slideToggle();
+});
+
+$('.open-btn.double').on('click', function(e) {
+	e.preventDefault;
+	$(this).parent().toggleClass('active');
+	$('.tarif_text.double').slideToggle();
+});
+
+$('.car_select-wrapper').on('click', function(e) {
+	e.preventDefault;
+	$(this).toggleClass('active');
+});
 
 $('.examples__slider').slick({
 	dots: true,
@@ -41,4 +58,31 @@ $('.examples__slider').slick({
 		// settings: "unslick"
 		// instead of a settings object
 	  ]
+});
+
+$('.reviews_slider').slick({
+	dots: true,
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	responsive: [
+		{
+		  breakpoint: 1110,
+		  settings: {
+			slidesToShow: 2,
+			slidesToScroll: 2
+		  }
+		},
+		{
+		  breakpoint: 600,
+		  settings: {
+			slidesToShow: 1,
+			slidesToScroll: 1
+		  }
+		}
+		// You can unslick at a given breakpoint now by adding:
+		// settings: "unslick"
+		// instead of a settings object
+	  ]
+
 });
